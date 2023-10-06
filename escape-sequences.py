b72cc747ec345d6de176e587e77f457a310a250e
Gigglich <vamaev8235@gmail.com>
2023-10-06 21:47:08 +0300

    закончил разметку двух дз

---
+++ escape-sequences.py
@@ -0,0 +1,14 @@
+print("Escape sequences\
+      \n\
+      \n\\a Bell(alert)\
+      \n\\b Backspace\
+      \n\
+      \n\\n New line\
+      \n\
+      \n\\t Horizontal tab\
+      \n\
+      \n\\\ Backflash\ \
+      \n\
+      \n\\\"Double quotation mark\"\
+      \n\
+      \n\\' Single quotation mark '")
